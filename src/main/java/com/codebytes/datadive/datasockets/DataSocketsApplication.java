cb52358b589a83d728af8b3e812e336fc338f1ed
Mahesh Math <mahesh.math@gmail.com>
2022-08-21 01:35:33 +0530

    New repo added:

---
+++ src/main/java/com/codebytes/datadive/datasockets/DataSocketsApplication.java
@@ -0,0 +1,13 @@
+package com.codebytes.datadive.datasockets;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DataSocketsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DataSocketsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/codebytes/datadive/datasockets/DataSocketsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.codebytes.datadive.datasockets;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DataSocketsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
