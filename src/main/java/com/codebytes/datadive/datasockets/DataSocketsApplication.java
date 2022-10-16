7ea72ead3de8f29bb3251733ec6d6a6e70049200
Mahesh Math <mahesh.math@gmail.com>
2022-10-16 23:19:26 +0530

    SQL changes

--- src/main/java/com/codebytes/datadive/datasockets/DataSocketsApplication.java
+++ src/main/java/com/codebytes/datadive/datasockets/DataSocketsApplication.java
@@ -7,6 +7,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class DataSocketsApplication {
 
 	public static void main(String[] args) {
+		System.out.println("Inside SpringBoot Main");
 		SpringApplication.run(DataSocketsApplication.class, args);
 	}
 
