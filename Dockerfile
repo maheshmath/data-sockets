fb2d56d08ef3e5f9f8520607b1a786018704a4c8
maheshmath <mahesh.math@gmail.com>
2022-11-01 11:43:40 +0530

    Spring profile

--- Dockerfile
+++ Dockerfile
@@ -1,4 +1,4 @@
 FROM openjdk:11.0.11-jre-slim
 COPY target/data-sockets-0.0.1-SNAPSHOT.jar app.jar
 EXPOSE 9091
-ENTRYPOINT ["java", "-jar", "app.jar"]
+ENTRYPOINT ["java", "-jar", "app.jar","--spring.profiles.active=prod"]
--- src/main/java/com/codebytes/datadive/datasockets/DataSocketsApplication.java
+++ src/main/java/com/codebytes/datadive/datasockets/DataSocketsApplication.java
@@ -1,14 +1,70 @@
 package com.codebytes.datadive.datasockets;
 
-import org.springframework.boot.SpringApplication;
+import java.util.Arrays;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Profile;
+import org.springframework.core.env.Environment;
+import org.springframework.stereotype.Component;
+
+import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.spi.DocumentationType;
+import springfox.documentation.spring.web.plugins.Docket;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
 @SpringBootApplication
+@EnableSwagger2
 public class DataSocketsApplication {
 
 	public static void main(String[] args) {
 		System.out.println("Inside SpringBoot Main");
-		SpringApplication.run(DataSocketsApplication.class, args);
+		new SpringApplicationBuilder(DataSocketsApplication.class).profiles("dev")
+		.run(args);
 	}
 
+	@Component
+	class MyRunner implements CommandLineRunner {
+
+		@Autowired
+		private Environment environment;
+
+		@Override
+		public void run(String... args) throws Exception {
+
+			System.out.println("Active profiles: " +
+					Arrays.toString(environment.getActiveProfiles()));
+		}
+	}
+
+	@Component
+	@Profile(value="dev")
+	class MyRunner2 implements CommandLineRunner {
+
+		@Override
+		public void run(String... args) throws Exception {
+
+			System.out.println("In development");
+		}
+	}
+
+	@Component
+	@Profile(value="prod")
+	class MyRunner3 implements CommandLineRunner {
+
+		@Override
+		public void run(String... args) throws Exception {
+
+			System.out.println("In production");
+		}
+	}
+
+	@Bean
+    public Docket productApi() {
+      return new Docket(DocumentationType.SWAGGER_2).select()
+         .apis(RequestHandlerSelectors.basePackage("com.tutorialspoint.swaggerdemo")).build();
+   }
 }
